d81b7a849fd92ff01055c49b8931a2083f1a0337
horsespirit <trednews@gmail.com>
2026-01-04 08:32:06 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,30 @@
+import requests
+
+def preleva():
+    # Questo è l'indirizzo di FlippingPal per l'Europa
+    url = "https://api.flippingpal.com/retail/market/ah-data-undercut/eu"
+    
+    # Queste sono le "chiavi" per entrare (il tuo Token)
+    headers = {
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTRmOGYxZjFlNmI0MTM3ZTQ2ODMwY2UiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzY3NTA4NDAwLCJleHAiOjE3Njc1MTIwMDB9.DIJmaM9wMVCC1-YJFpy50SiCO0UyWPXhN42EIaM_Ynk',
+        'user-agent': 'Mozilla/5.0'
+    }
+    
+    # Questa è la lista dei tuoi server (Realms)
+    payload = {
+        "retailSellingRealms": [3679,1325,3713,1104,3686,512,1302,3702,578,1406,1597,1417,580,581,1416,1393,1307,1127,1084,531,1618,1121,1403,1092,1378,3691,1315,1091,3692,1385,3703,509,1401,3657,1303,1587,1390,1596,1305,1080,3690,633,1082,1388,570,3681,1098,1621,1085,1624,1105,612,1316,3696,1408,1301,1309,1396,3682,1329,1099,1096,3666,1598,3391,3656,2073,1331,2074,604,1405,1402,3674,1384,1122,1400,510,1313,1097,1335,1379],
+        "retailBuyingRealms": [3679,1325,3713,1104,3686,512,1302,3702,578,1406,1597,1417,580,581,1416,1393,1307,1127,1084,531,1618,1121,1403,1092,1378,3691,1315,1091,3692,1385,3703,509,1401,3657,1303,1587,1390,1596,1305,1080,3690,633,1082,1388,570,3681,1098,1621,1085,1624,1105,612,1316,3696,1408,1301,1309,1396,3682,1329,1099,1096,3666,1598,3391,3656,2073,1331,2074,604,1405,1402,3674,1384,1122,1400,510,1313,1097,1335,1379],
+        "userId": "694f8f1f1e6b4137e46830ce"
+    }
+    
+    print("Avvio download dati WoW (Cloud)...")
+    # Il timeout è alto perché i dati sono tanti
+    r = requests.post(url, headers=headers, json=payload, timeout=300)
+    
+    if r.status_code == 200:
+        print(f"✅ SUCCESSO! Scaricati dati per {len(r.json())} oggetti.")
+    else:
+        print(f"❌ Errore server: {r.status_code}")
+
+if __name__ == "__main__":
+    preleva()
